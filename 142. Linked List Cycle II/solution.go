1bcd55959a20e46b496b9a3db0b813e6cecf47c8
Craig Chi <craig10624@gmail.com>
2019-06-29 00:24:51 +0800

    142. Linked List Cycle II

---
+++ 142. Linked List Cycle II/solution.go
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func detectCycle(head *ListNode) *ListNode {
+	fast, slow := head, head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if fast == slow {
+			break
+		}
+	}
+	if fast == nil || fast.Next == nil {
+		return nil
+	}
+	for fast = head; fast != slow; fast, slow = fast.Next, slow.Next {
+	}
+	return fast
+}
